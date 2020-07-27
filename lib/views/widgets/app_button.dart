67f14240ac31ad465abd3dda50fdd271158ee652
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-07-27 23:16:09 +0100

    Changed container to button widget

--- lib/views/widgets/app_button.dart
+++ lib/views/widgets/app_button.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
-
   final VoidCallback onTap;
   final String content;
 
-  AppButton({this.onTap,this.content});
+  AppButton({this.onTap, this.content});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        height: 55,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Center(
-          child: Text(
-            content,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-            ),
+    return SizedBox(
+      height: 55,
+      child: FlatButton(
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            10,
           ),
+        ),
+        color: Colors.blue,
+        textColor: Colors.white,
+        onPressed: onTap,
+        child: Center(
+          child: Text(content),
         ),
       ),
     );
